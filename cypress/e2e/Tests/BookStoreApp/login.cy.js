5be97ae81988bf9be809e14334a0ec4fee4580fc
camico2023lili <lcamico@banksa.com.ar>
2023-09-22 17:50:58 -0300

    Cambio por falla server other SUT

---
+++ cypress/e2e/Tests/BookStoreApp/login.cy.js
@@ -0,0 +1,9 @@
+describe('WS-Create Profile', () => {
+  beforeEach('PRC: Enter in Page', () => {
+    cy.visit(dataLogin.baseUrl);
+  });
+  it('Crear Perfil', () => {
+    expect(true).to.equal(true);
+  });
+});
+import dataLogin from '../../../fixtures/login.json';

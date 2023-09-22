71ed4acacadceb94200b70f648fa8e07964294d6
camico2023lili <lcamico@banksa.com.ar>
2023-09-22 18:43:20 -0300

    Cambios realizados correctamente con CI

--- cypress/e2e/test.cy.js
+++
@@ -1,8 +0,0 @@
-describe("prueba", () => {
-	beforeEach("PRC: Enter in Page", () => {
-		cy.visit("https://www.saucedemo.com/");
-	});
-	it("Iniciar sesión", () => {
-		expect("true").to.equal("true");
-	});
-});

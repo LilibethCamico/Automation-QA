a704fbe2da5ef867ce1f5f0002098dda9d11ed3e
camico2023lili <lcamico@banksa.com.ar>
2023-09-25 13:40:20 -0300

    Reajuste por tocar algunas configuraciones, todo OK

--- cypress/e2e/Tests/BookStoreApp/login.cy.js
+++ cypress/e2e/Tests/BookStoreApp/login.cy.js
@@ -1,9 +1,11 @@
 describe('WS-Create Profile', () => {
-  beforeEach('PRC: Enter in Page', () => {
-    cy.visit(dataLogin.baseUrl);
-  });
-  it('Crear Perfil', () => {
-    expect(true).to.equal(true);
-  });
+	beforeEach('PRC: Enter in Page', () => {
+		cy.visit(dataLogin.baseUrl);
+	});
+	it('Crear Perfil', () => {
+		expect(true).to.equal(true);
+	});
 });
 import dataLogin from '../../../fixtures/login.json';
+import { removeLogs } from '../../../support/helper/RemoveLogs';
+removeLogs();

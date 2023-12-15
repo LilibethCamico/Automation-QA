78621f05147cac6055c95244fc816593060b3535
LilibethCamico <argcamico@gmail.com>
2023-12-15 00:48:42 -0300

    Update: Cambios debido a que la otra url estaba caido el servidor y para hacer una pequeña prueba lo cambie

---
+++ cypress/e2e/Tests/blasedemoApp/Reserva.cy.js
@@ -0,0 +1,27 @@
+describe('WS-Create Profile', () => {
+	beforeEach('PRC: Enter in Page', () => {
+		cy.visit(data.baseUrl);
+	});
+	it('Access', () => {
+		expect(true).to.equal(true);
+	});
+	it('Choose departure and destination city', () => {
+		//cy.get('select[name="fromPort"]').select('Boston');
+		cy.get('select[name="fromPort"] option').then((options) => {
+			const randomIndex = Cypress._.random(0, options.length - 1);
+			const randomOption = options[randomIndex];
+			cy.get('select[name="fromPort"]').select(randomOption.value);
+		});
+		//cy.get('select[name="toPort"]').select('Berlin');
+		cy.get('select[name="toPort"] option').then((options) => {
+			const randomIndex = Cypress._.random(0, options.length - 1);
+			const randomOption = options[randomIndex];
+			cy.get('select[name="toPort"]').select(randomOption.value);
+		});
+
+
+	});
+});
+import data from '../../../fixtures/data.json';
+import { removeLogs } from '../../../support/helper/RemoveLogs';
+removeLogs();
